eadfff99f5c2b7cadf69efba3d31a24d50aa3c9a
Marissa Skudlarek <marissa.skudlarek@gmail.com>
2019-05-16 21:34:02 +0000

    Started creating HTML templates and Python server

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from jinja2 import StrictUndefined
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import Decade, Country, Book, connect_to_db, db
+
+
 app = Flask(__name__)
 
+app.secret_key = "ABC"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return render_template("index.html")
