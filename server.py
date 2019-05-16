d3c3878ce8f8ff85f69ed514a3c12e3054746799
Marissa Skudlarek <marissa.skudlarek@gmail.com>
2019-05-16 22:43:21 +0000

    Working on server functions and input form

--- server.py
+++ server.py
@@ -5,13 +5,59 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from model import Decade, Country, Book, connect_to_db, db
 
+import re
+import pickle
 
 app = Flask(__name__)
 
 app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
+
+def remove_irrelevant_characters(text_string):
+    '''Removes commas, colons, semicolons, parentheses, double quotes, underscores, and
+    asterisks from text. Replaces long dashes with a single space.'''
+
+    text_string = re.sub(",|;|\*|_|\"|\(|\)|:|\”|\“", "", text_string)
+    return text_string.replace("--", " ")
+
+
 @app.route('/')
 def index():
     """Homepage."""
-    return render_template("index.html")
+    decades = db.session.query(Decade.decade).all()
+
+     #Is there a less clunky way to write this?
+
+    formatted_decades = []
+    for decade in decades:
+        formatted = decade[0]
+        formatted_decades.append(formatted)
+
+    return render_template("index.html", decades=formatted_decades)
+
+
+@app.route('/results')
+def see_results():
+    return render_template("results.html")
+
+
+@app.route('/process-text')
+def analyzer():
+    textstring = remove_irrelevant_characters(request.args["textstring"])
+    print(textstring)
+
+    if request.args["analysis-type"] == "words":
+        pass
+
+    return redirect("/results")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
