6197d520cd1954a8555fe6cd223485fb7972e99d
Marissa Skudlarek <marissa.skudlarek@gmail.com>
2019-06-14 22:18:06 +0000

    More styling. Rewrote tests to match current html.

--- test_server.py
+++ test_server.py
@@ -44,32 +44,32 @@ class TestFlaskRoutes(unittest.TestCase):
     def test_index(self):
         """Make sure index page returns correct HTML."""
 
-        result = self.client.get('/')
-        self.assertIn(b'anachronism finder!', result.data)
+        result = self.client.get("/")
+        self.assertIn(b"Measure", result.data)
 
 
     def test_word_search(self):
 
-        result = self.client.get('/word-search')
-        self.assertIn(b'2500 characters', result.data)
+        result = self.client.get("/word-search")
+        self.assertIn(b"2500 characters", result.data)
 
 
     def test_bigram_search(self):
 
-        result = self.client.get('/bigram-search')
-        self.assertIn(b'input a bigram', result.data)
+        result = self.client.get("/bigram-search")
+        self.assertIn(b"specific bigram", result.data)
 
 
     def test_show_corpus(self):
 
-        result = self.client.get('/corpus')
-        self.assertIn(b'Currently, our corpus', result.data)
+        result = self.client.get("/corpus")
+        self.assertIn(b"Glimpses", result.data)
 
 
     def test_show_methodology(self):
 
-        result = self.client.get('/methodology')
-        self.assertIn(b'Kowal', result.data)
+        result = self.client.get("/faqs")
+        self.assertIn(b"Kowal", result.data)
 
 
 if __name__ == '__main__':
