cfac2332654752a99085cc25d8abbc38b6d5a4ad
Marissa Skudlarek <marissa.skudlarek@gmail.com>
2019-06-23 19:47:30 +0000

    Added github & twitter links to footer

--- textprocessor.py
+++ textprocessor.py
@@ -72,9 +72,9 @@ def prepare_texts(startrange, endrange):
         text = open_and_read_file(f"full_texts/book{i}_full.txt")
         pickle_data(f"word_sets/book{i}_set.pickle", make_unique_word_set(text))
         print(f"Made set for book {i}")
-        # pickle_data(f"bigram_dicts/book{i}_dict.pickle", make_bigram_freq_dict(make_bigrams(text)))
-        # print(f"Made dict for book {i}")
+        pickle_data(f"bigram_dicts/book{i}_dict.pickle", make_bigram_freq_dict(make_bigrams(text)))
+        print(f"Made dict for book {i}")
 
 
 if __name__ == "__main__":
-    prepare_texts(1, 44)
+    prepare_texts(43, 44)
